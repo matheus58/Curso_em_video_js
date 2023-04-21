7a18185022e60d5860b77a653d8a068317705190
matheus58 <vinyciusmatheus58@gmail.com>
2023-04-21 19:42:13 -0300

    estrutura de repetiçao

---
+++ Aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+if(vel >= 60){// CONDISAO SIMPLES
+    console.log(`Voce ultrapasou a velocidade permitida. MULTADO!`)
+}
+console.log(`A velocidede do seu carro e ${vel}km/h`)
---
+++ Aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'china'
+console.log(`vivendo no  ${pais}`)
+
+if (pais == 'Brasil' ){//composta
+    console.log('Voce e Brasileiro')
+}else{
+    console.log('voce e estrangeiro')
+}
